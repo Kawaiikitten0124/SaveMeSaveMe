a2d9e28c68f780ed66a47aa64110597c89ee757f
Kawaiikitten0124 <apoultne@student.fitchburgstate.edu>
2024-11-26 10:59:37 -0500

    stupid tech

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float forwardInput;
+    public float horizontalInput;
+    public float speed = 10.0f;
+    private Rigidbody2D playerRb;
+    public float jumpForce = 1;
+    public float gravityModifier;
+    public GameObject projectilePrefab;
+    //private bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      playerRb = GetComponent<Rigidbody2D>();
+     // Physics2D.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //player movement and keep in bounds
+        if(transform.position.x < -7)
+        {
+            transform.position = new Vector3(-7, transform.position.y, transform.position.z);
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+
+       // forwardInput = Input.GetAxis("Vertical");
+       // transform.Translate(Vector3.up * Time.deltaTime * speed * forwardInput);
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);        
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            //arrow shoot
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+    }
+
+     
+
+
+
+}
+
+
+
